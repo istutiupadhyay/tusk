19d27eb94264e83e41392b77d3a7c6c98be8c1bd
istutiupadhyay <66789421+istutiupadhyay@users.noreply.github.com>
2023-01-20 15:32:56 +0530

    Update products.js

--- models/products.js
+++ models/products.js
@@ -1,21 +1,5 @@
-const fs = require('fs');
-const path = require('path');
-
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
-);
-
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
+const db = require('../util/database');
+const Cart = require('./cart');
 
 module.exports = class Product {
   constructor(title, imageUrl, description, price) {
@@ -26,34 +10,21 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
-    getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
-    });
+    return db.execute(
+      'INSERT INTO products(title, price, imageUrl, description) VALUES(?, ?, ?, ?)',
+      [this.title, this.price, this.imageUrl, this.description]
+    );
   }
+  
   static deleteById(id){
-    getProductsFromFile(products => {
-      const product = products.find(prod => prod.id === id);
-      const updatedProducts = products.filter(prod => prod.id !== id);
-      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-        if(!err) {
-          cart.deleteProduct(id, product.price);
-        };
-      });
-    });
+     return db.execute('DELETE FROM products WHERE products.id = ?', [id]);
   }
 
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
+  static fetchAll() {
+    return db.execute('SELECT * FROM products');
   }
-
-  static fetchAll(cb){
-    getProductFromFile(products => {
-        const product = products.find(p => p.id === id);
-        cb(product);
-    });
-  };
+    
+  static findById(id) {
+    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
+  }
 };
